018c698dd5ac7515326740ae3d27d259174da34c
Algunenano <algunenano@gmail.com>
2015-11-10 17:31:16 +0100

    Main Window testing

--- Emoti-cli-app/src/mainwindow.cpp
+++ Emoti-cli-app/src/mainwindow.cpp
@@ -1,14 +1,70 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "network.h"
+#include <QDate>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow)
+    ui(new Ui::MainWindow),
+    cam(),
+    camListPosition(0)
 {
-    ui->setupUi(this);
+    this->ui->setupUi(this);
+
+    //Camera
+    this->cameraList_Setup();
+    this->connect(this->ui->camListBox, SIGNAL(currentIndexChanged(int)), this, SLOT(cameraList_Change()));
+
+    //Button
+    this->connect(this->ui->buttonSend, SIGNAL(clicked(bool)), this, SLOT(getImageAndSend()));
+
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::cameraList_Setup()
+{
+    this->camList = QCameraInfo::availableCameras();
+
+    for (QCameraInfo &cameraInfo : camList)
+    {
+        this->ui->camListBox->addItem(cameraInfo.deviceName());
+        qDebug() << Q_FUNC_INFO << "Detected device: " << cameraInfo.deviceName();
+    }
+
+    if (!this->camList.isEmpty())
+    {
+        this->cameraList_Change();
+    }
+
+}
+
+void MainWindow::cameraList_Change()
+{
+    int ind = this->ui->camListBox->currentIndex();
+
+    if (ind >= this->camList.size())
+    {
+        qDebug() << Q_FUNC_INFO << "Invalid index";
+        return;
+    }
+
+    QCameraInfo qi = this->camList.at(ind);
+    this->cam.setup(qi); //TODO handle errors
+}
+
+void MainWindow::getImageAndSend()
+{
+    QString name = qgetenv("USER");
+    if (name.isEmpty())
+        name = qgetenv("USERNAME");
+    auto img = cam.captureImageSync("PNG");
+
+    QDateTime time = QDateTime::currentDateTime();
+    Network net("http://10.102.83.80:8080/imagepost");
+    net.sendImage(img, name, time.toString());
 }
